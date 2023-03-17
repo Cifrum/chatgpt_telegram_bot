40b438dd71598a9a141b3a8c40a76cb75ca1c190
kkiniuss <kkiniuss03@gmail.com>
2023-03-17 03:26:37 +0300

    fix

--- bot/bot.py
+++ bot/bot.py
@@ -311,7 +311,7 @@ async def show_balance_handle(update: Update, context: CallbackContext):
     subscribe_date = db.get_user_attribute(user_id, "subscribe_date")
     if (is_subscribe and datetime.timestamp(datetime.now()) - datetime.timestamp(subscribe_date) < 0):
         subscribe_date = db.get_user_attribute(user_id, "subscribe_date")
-        text = f"Поздравляю, у тебя активирована подписка!\nОна действует до {subscribe_date}"
+        text = f"Поздравляю, у тебя активирована подписка!\nОна действует до {subscribe_date.strftime('%d-%m-%Y, %H:%M:%S')}"
     else:
         keyboard = []
         keyboard.append([InlineKeyboardButton('Купить подписку❤', callback_data=f"buy_subscribe")])
